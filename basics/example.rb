c8227454cd0da41ee7321dfc069717eaff081d54
Zuzanna Stolińska <zuzanna.st@gmail.com>
2014-11-03 15:57:26 +0100

    [#7] Add methods long_lines, length, truncate and contain

--- basics/example.rb
+++ basics/example.rb
@@ -3,39 +3,39 @@ class Article
   attr_accessor :likes, :dislikes, :points, :votes
 
   def initialize(title, body, author=nil)
-  	@title = "New OSX"
-  	@body = "Some text about new OSX"
-  	@author = author
+    @title = "New OSX"
+    @body = "Some text about new OSX"
+    @author = author
 
-  	@created_at = Time.now
-  	@likes = 0
+    @created_at = Time.now
+    @likes = 0
     @dislikes = 0
   end
 
   def like!
-  	self.likes += 1
+    self.likes += 1
   end
 
   def dislike!
-  	self.dislikes += 1
+    self.dislikes += 1
   end
 
   def points
-  	@points = likes - dislikes
+    @points = likes - dislikes
   end
 
   def votes
-  	@votes = likes + dislikes
+    @votes = likes + dislikes
   end
 
   def long_lines   
     long_line = []
 
-    body.lines.each do |line|
-      if line.length > 80
-        line
-      end
+    body.each_line do |line|  
+      long_line << line
     end
+
+    long_line.select { |line| line.length > 80 }
   end
 
   def length
@@ -43,13 +43,21 @@ class Article
   end
 
   def truncate(limit)
-  	if @body.length > limit
-      @body.truncate
-  	end
+    limit = limit - 3
+    if body.length > limit
+      body.slice!(limit..body.length)
+      body << ('...')
+    else
+      body
+    end
   end
 
   def contain?(value)
-    true
+    if body.match(value) 
+      return true
+    else
+      return false
+    end
   end
 
 end
