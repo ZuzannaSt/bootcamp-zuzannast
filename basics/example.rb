7a7adda0d1f20766a6c39a3d38c1a138b77989e8
Zuzanna Stolińska <zuzanna.st@gmail.com>
2014-11-03 12:21:06 +0100

    [#4] Add attr_accessor to the Article class

--- basics/example.rb
+++ basics/example.rb
@@ -1,20 +1,34 @@
 class Article
-	def title
-		@title
-	end
-	def body
-		@body
-	end
-	def author
-		@author
-	end
-	def created_at
-		@created_at
-	end
-	def likes(value)
-		@likes = value
-	end
-	def dislikes(value)
-		@dislikes = value
-	end
+
+attr_reader :title
+attr_reader :body
+attr_reader :author
+attr_reader :created_at
+attr_accessor :likes
+attr_accessor :dislikes
+
+  def title
+    @title
+  end
+
+  def body
+    @body
+  end
+
+  def author
+    @author
+  end
+
+  def created_at
+    @created_at
+  end
+
+  def likes(value)
+    @likes = value
+  end
+
+  def dislikes(value)
+    @dislikes = value
+  end
+
 end
