c90df999c4b03f7b3ebfca2b2747cabe2ec9485f
Zuzanna Stolińska <zuzanna.st@gmail.com>
2014-11-05 13:02:15 +0100

    [#30] Add vatidations to Parking model

--- app/models/parking.rb
+++ app/models/parking.rb
@@ -1,4 +1,8 @@
 class Parking < ActiveRecord::Base
+	validates :places, presence: true
+	validates :hour_price, :day_price, presence: true, numericality: true
+	validates :kind, inclusion: { in: %w(outdoor indoor private strees) }
+
 	belongs_to :address
 	belongs_to :owner, class_name: "Person"
 	has_many :place_rents
