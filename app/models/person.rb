0ad69fd401ed020ec4c3106d8865ce39f3229c5a
Zuzanna Stolińska <zuzanna.st@gmail.com>
2014-11-05 16:04:36 +0100

    [#29] Add presence to validation

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,5 +1,5 @@
 class Person < ActiveRecord::Base
-	validates :first_name
+	validates :first_name, presence: true
 
 	has_many :parkings, foreign_key: "owner_id"
 	has_many :cars, foreign_key: "owner_id"
