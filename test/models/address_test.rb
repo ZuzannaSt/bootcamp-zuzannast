52fbd2fb4dd655e94ae579258d3cd085fbe00c8b
Zuzanna Stolińska <zuzanna.st@gmail.com>
2014-11-05 16:25:29 +0100

    [#27] Add fixcture values and tests for Address model

--- test/models/address_test.rb
+++ test/models/address_test.rb
@@ -1,7 +1,31 @@
 require 'test_helper'
 
 class AddressTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+  	@brzesko = addresses(:brzesko)
+  end
+
+  test "should not save address without city" do
+  	@brzesko.city = nil
+  	assert_not @brzesko.valid?
+  	assert_not @brzesko.errors[:city].empty?
+  end
+
+  test "should not save address without street" do
+  	@brzesko.street = nil
+  	assert_not @brzesko.valid?
+  	assert_not @brzesko.errors[:street].empty?
+  end
+
+  test "should not save address without zip_code" do
+  	@brzesko.zip_code = nil
+  	assert_not @brzesko.valid?
+  	assert_not @brzesko.errors[:zip_code].empty?
+  end
+  
+  test "should not save address with wrong zip_code" do
+  	@brzesko.zip_code = "2222222"
+  	assert_not @brzesko.valid?
+  end
+
 end
