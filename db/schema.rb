4fb93ccea89fc43c29ed0407282b7bd03ba428c3
Zuzanna Stolińska <zuzanna.st@gmail.com>
2014-11-05 12:26:18 +0100

    [#26] Add timestamps to parkings table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141105102844) do
+ActiveRecord::Schema.define(version: 20141105112435) do
 
   create_table "addresses", force: true do |t|
     t.string   "city"
@@ -30,12 +30,14 @@ ActiveRecord::Schema.define(version: 20141105102844) do
   end
 
   create_table "parkings", force: true do |t|
-    t.integer "places"
-    t.string  "kind"
-    t.decimal "hour_price"
-    t.decimal "day_price"
-    t.integer "owner_id"
-    t.integer "address_id"
+    t.integer  "places"
+    t.string   "kind"
+    t.decimal  "hour_price"
+    t.decimal  "day_price"
+    t.integer  "owner_id"
+    t.integer  "address_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "people", force: true do |t|
