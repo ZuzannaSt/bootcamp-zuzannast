aeaa14e3e53949874afa58b1c388633e203dba1f
Zuzanna Stolińska <zuzanna.st@gmail.com>
2014-11-06 16:42:55 +0100

    [#39] Add helper for current_person

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   helper_method :page_title
+  helper_method :current_person
 
   def page_title
     controller_name
+  end
+
+  def current_person
+    Person.first
   end
 end
