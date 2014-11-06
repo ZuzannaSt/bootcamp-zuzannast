1be6444dc5b6e1ad087c92dee0691be9f4b85546
Zuzanna Stolińska <zuzanna.st@gmail.com>
2014-11-07 00:01:52 +0100

    [#40] Removed generated in scaffold responds_to format and changed associations to use current_user method.

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -24,31 +24,22 @@ class CarsController < ApplicationController
   # POST /cars
   # POST /cars.json
   def create
-    @car = Car.new(car_params)
-    @car.owner = current_person
+    @car = current_person.cars.build(car_params)
 
-    respond_to do |format|
-      if @car.save
-        format.html { redirect_to @car, notice: 'Car was successfully created.' }
-        format.json { render :show, status: :created, location: @car }
-      else
-        format.html { render :new }
-        format.json { render json: @car.errors, status: :unprocessable_entity }
-      end
+    if @car.save
+      redirect_to @car, notice: 'Car was successfully created.'
+    else
+      render :new
     end
   end
 
   # PATCH/PUT /cars/1
   # PATCH/PUT /cars/1.json
   def update
-    respond_to do |format|
-      if @car.update(car_params)
-        format.html { redirect_to @car, notice: 'Car was successfully updated.' }
-        format.json { render :show, status: :ok, location: @car }
-      else
-        format.html { render :edit }
-        format.json { render json: @car.errors, status: :unprocessable_entity }
-      end
+    if @car.update(car_params)
+      redirect_to @car, notice: 'Car was successfully updated.'
+    else
+      render :edit
     end
   end
 
@@ -56,16 +47,13 @@ class CarsController < ApplicationController
   # DELETE /cars/1.json
   def destroy
     @car.destroy
-    respond_to do |format|
-      format.html { redirect_to cars_url, notice: 'Car was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to cars_url, notice: 'Car was successfully destroyed.'
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_car
-      @car = Car.find(params[:id])
+      @car = current_person.cars.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
