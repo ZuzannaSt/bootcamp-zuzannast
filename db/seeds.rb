15123e0f9d674c0f9dc353757e0a472891c70d8b
Zuzanna Stolińska <zuzanna.st@gmail.com>
2014-11-06 16:41:23 +0100

    [#39] Create person in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+person = Person.create([{ first_name: 'Steve'}, { last_name: 'Jobs'}])
