85123348e08dce5a842a86c50b05f494df78833c
Zuzanna Stolińska <zuzanna.st@gmail.com>
2014-11-06 09:49:50 +0100

    [#31] Add fixtures and tests for Place_rent model

--- test/models/place_rent_test.rb
+++ test/models/place_rent_test.rb
@@ -1,7 +1,40 @@
 require 'test_helper'
 
 class PlaceRentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+  	@hour = place_rent(:hour)
+  end
+
+  test "should save place_rent" do
+  	assert @hour.valid?
+  	assert @hour.errors[:start_date].empty?
+  	assert @hour.errors[:end_date].empty?
+  	assert @hour.errors[:parking].empty?
+  	assert @hour.errors[:car].empty?
+   end
+
+  test "should not save place_rent without start_date" do
+  	@hour.start_date = nil
+  	assert @hour.invalid?
+  	assert !@hour.errors[:start_date].empty?
+  end
+
+  test "should not save place_rent without end_date" do
+  	@hour.end_date = nil
+  	assert @hour.invalid?
+  	assert !@hour.errors[:end_date].empty?
+  end
+
+  test "should not save place_rent without parking" do
+  	@hour.parking = nil
+  	assert @hour.invalid?
+  	assert !@hour.errors[:parking].empty?
+  end
+
+  test "should not save place_rent without car" do
+  	@hour.car = nil
+  	assert @hour.invalid?
+  	assert !@hour.errors[:car].empty?
+  end
+
 end
