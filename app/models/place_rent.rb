4861b46f315a7ef54559fe0ce88a79d293c603cd
Zuzanna Stolińska <zuzanna.st@gmail.com>
2014-11-07 12:29:01 +0100

    [#31] Changes validation to parking and car instead of ids and removed relation with person.

--- app/models/place_rent.rb
+++ app/models/place_rent.rb
@@ -1,6 +1,5 @@
 class PlaceRent < ActiveRecord::Base
-	validates :start_date, :end_date, :parking_id, :car_id, presence: true
+	validates :start_date, :end_date, :parking, :car, presence: true
 	belongs_to :parking
 	belongs_to :car
-	has_many :people
 end
