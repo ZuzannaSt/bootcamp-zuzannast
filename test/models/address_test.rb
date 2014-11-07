175436f2a986624516b33758d2b1f198826054ad
Zuzanna Stolińska <zuzanna.st@gmail.com>
2014-11-07 12:17:57 +0100

    [#27] Converted tabs to spaces

--- test/models/address_test.rb
+++ test/models/address_test.rb
@@ -2,38 +2,36 @@ require 'test_helper'
 
 class AddressTest < ActiveSupport::TestCase
   def setup
-  	@brzesko = addresses(:brzesko)
+    @brzesko = addresses(:brzesko)
   end
 
   test "should save address" do
-  	assert @brzesko.valid?
-  	assert @brzesko.errors[:city].empty?
-  	assert @brzesko.errors[:street].empty?
-  	assert @brzesko.errors[:zip_code].empty?
+    assert @brzesko.valid?
+    assert @brzesko.errors[:city].empty?
+    assert @brzesko.errors[:street].empty?
+    assert @brzesko.errors[:zip_code].empty?
   end
 
-
   test "should not save address without city" do
-  	@brzesko.city = nil
-  	assert_not @brzesko.valid?
-  	assert_not @brzesko.errors[:city].empty?
+    @brzesko.city = nil
+    assert_not @brzesko.valid?
+    assert_not @brzesko.errors[:city].empty?
   end
 
   test "should not save address without street" do
-  	@brzesko.street = nil
-  	assert_not @brzesko.valid?
-  	assert_not @brzesko.errors[:street].empty?
+    @brzesko.street = nil
+    assert_not @brzesko.valid?
+    assert_not @brzesko.errors[:street].empty?
   end
 
   test "should not save address without zip_code" do
-  	@brzesko.zip_code = nil
-  	assert_not @brzesko.valid?
-  	assert_not @brzesko.errors[:zip_code].empty?
+    @brzesko.zip_code = nil
+    assert_not @brzesko.valid?
+    assert_not @brzesko.errors[:zip_code].empty?
   end
   
   test "should not save address with wrong zip_code" do
-  	@brzesko.zip_code = "2222222"
-  	assert_not @brzesko.valid?
+    @brzesko.zip_code = "2222222"
+    assert_not @brzesko.valid?
   end
-
 end
