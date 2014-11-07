0befc65c56487a3d5858d2b38e4bb1ccd085a79f
Zuzanna Stolińska <zuzanna.st@gmail.com>
2014-11-07 12:25:48 +0100

    [#30] Converted tab to spaces

--- test/models/parking_test.rb
+++ test/models/parking_test.rb
@@ -2,49 +2,49 @@ require 'test_helper'
 
 class ParkingTest < ActiveSupport::TestCase
   def setup
-  	@outdoor = parking(:outdoor)
+    @outdoor = parking(:outdoor)
   end
 
   test "should save parking" do
-  	assert @outdoor.valid?
-  	assert @outdoor.errors[:places].empty?
-  	assert @outdoor.errors[:hour_price].empty?
-  	assert @outdoor.errors[:day_price].empty?
-  	assert @outdoor.errors[:kind].empty?
+    assert @outdoor.valid?
+    assert @outdoor.errors[:places].empty?
+    assert @outdoor.errors[:hour_price].empty?
+    assert @outdoor.errors[:day_price].empty?
+    assert @outdoor.errors[:kind].empty?
   end
 
   test "should not save parking without places" do
-  	@outdoor.places = nil
-  	assert @outdoor.invalid?
-  	assert !@outdoor.errors[:places].empty?
+    @outdoor.places = nil
+    assert @outdoor.invalid?
+    assert !@outdoor.errors[:places].empty?
   end
 
   test "should not save parking without kind" do
-  	@outdoor.kind = nil
-  	assert_not @outdoor.valid?
-  	assert_not @outdoor.errors[:kind].empty?
+    @outdoor.kind = nil
+    assert_not @outdoor.valid?
+    assert_not @outdoor.errors[:kind].empty?
   end
 
   test "should not save parking without hour_price" do
-  	@outdoor.hour_price = nil
-  	assert @outdoor.invalid?
-  	assert !@outdoor.errors[:hour_price].empty?
+    @outdoor.hour_price = nil
+    assert @outdoor.invalid?
+    assert !@outdoor.errors[:hour_price].empty?
   end
 
   test "should not save parking without day_price" do
-  	@outdoor.day_price = nil
-  	assert @outdoor.invalid?
-  	assert !@outdoor.errors[:day_price].empty?
+    @outdoor.day_price = nil
+    assert @outdoor.invalid?
+    assert !@outdoor.errors[:day_price].empty?
   end
 
   test "should have numeric hour_price" do
-  	@outdoor.hour_price = "lalala"
-  	assert @outdoor.invalid?
-  	assert !@outdoor.errors[:hour_price].empty?
+    @outdoor.hour_price = "lalala"
+    assert @outdoor.invalid?
+    assert !@outdoor.errors[:hour_price].empty?
   end
 
   test "kind should have value from list" do
-  	@outdoor.kind = "something"
-  	assert @outdoor.invalid?
+    @outdoor.kind = "something"
+    assert @outdoor.invalid?
   end
 end
