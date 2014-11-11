c39dc718eaf09a717a07336c47268fe86c4035c5
Zuzanna Stolińska <zuzanna.st@gmail.com>
2014-11-11 10:06:35 +0100

    [#58] Displaying full name of current person in the navbar

--- app/models/person.rb
+++ app/models/person.rb
@@ -3,4 +3,12 @@ class Person < ActiveRecord::Base
 
   has_many :parkings, foreign_key: "owner_id"
   has_many :cars, foreign_key: "owner_id"
+
+  def full_name
+    if last_name.blank?
+      "#{self.first_name}"
+    else
+      "#{self.first_name} #{self.last_name}"
+    end
+  end
 end
