aaabc441cbb972cdfb6184b22ed0e6c62e99f1b2
Zuzanna Stolińska <zuzanna.st@gmail.com>
2014-11-13 10:34:34 +0100

    [#73] Send welcome email when user registers

---
+++ app/mailers/registration_mailer.rb
@@ -0,0 +1,17 @@
+class RegistrationMailer < ActionMailer::Base
+  default from: "hello@bookparking.dev"
+  
+  def welcome_email(person)
+    @person = person
+    @url  = login_url(:host)
+    mail(to: @person.account.email, subject: 'Welcome to Bookparking')
+  end
+
+  def default_url_options
+    if Rails.env.production?
+      { :host => "bookpark.dev"}
+    else
+      {:host => "bookpark.dev"}
+    end
+  end
+end
