d674767282909cf947bb193124cfdebaaddc6a1b
Zuzanna Stolińska <zuzanna.st@gmail.com>
2014-11-13 11:22:18 +0100

    [#73] Sending accounts controller that was forgotten earlier.

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -8,12 +8,13 @@ class AccountsController < ApplicationController
     @account = Account.new(account_params)
 
     if @account.save
+      RegistrationMailer.welcome_email(@account.person).deliver
       redirect_to root_path, notice: 'Account was successfully created.'
     else
       render action: 'new'
     end
   end
-
+  
   private
     def account_params
       params.require(:account).permit(:email, :password, :password_confirmation, person_attributes: [ :first_name, :last_name, ]) 
