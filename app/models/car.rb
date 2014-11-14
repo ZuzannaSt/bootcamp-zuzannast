e40386941e0e4a9479833219510756f9503d0303
Zuzanna Stolińska <zuzanna.st@gmail.com>
2014-11-14 09:33:45 +0100

    [#83] Added dragonfly extension to Car class only and added system files to git.ignore

--- app/models/car.rb
+++ app/models/car.rb
@@ -2,6 +2,7 @@ class Car < ActiveRecord::Base
   validates :model, :registration_number, :owner, presence: true
   dragonfly_accessor :image
 
+  extend Dragonfly::Model
   extend Dragonfly::Model::Validations
     validates_size_of :image, maximum: 200.kilobytes
     validates_property :format, of: :image, in: ['jpeg', 'png', 'gif']
