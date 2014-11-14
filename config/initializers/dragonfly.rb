f9926c0b74ad7b0d04a6f8720987347ed89b6bf2
Zuzanna Stolińska <zuzanna.st@gmail.com>
2014-11-14 10:13:41 +0100

    [#84] Changed bucket variable name.

--- config/initializers/dragonfly.rb
+++ config/initializers/dragonfly.rb
@@ -9,7 +9,7 @@ Dragonfly.app.configure do
   url_format "/media/:job/:name"
 
   datastore :s3,
-    bucket_name: ENV['AWS_BUCKET_NAME'],
+    bucket_name: ENV['S3_BUCKET_NAME'],
     access_key_id: ENV['AWS_ACCESS_KEY'],
     secret_access_key: ENV['AWS_SECRET_KEY']
 end
