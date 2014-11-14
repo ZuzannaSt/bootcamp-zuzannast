eaf6face1c9f8fa1a74becf02a89e4aa50245df9
Zuzanna Stolińska <zuzanna.st@gmail.com>
2014-11-14 17:01:49 +0100

    [#73] Changed ActionMailer settings in different environments.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -36,4 +36,8 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  ActionMailer::Base.delivery_method = :test
+
+  config.action_mailer.default_url_options = { :host => "bookparking.dev" }
 end
