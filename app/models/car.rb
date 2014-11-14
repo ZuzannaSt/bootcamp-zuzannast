a5fbe21622efbd480f2ab4ead8c280cab28fbdaf
Zuzanna Stolińska <zuzanna.st@gmail.com>
2014-11-14 09:50:43 +0100

    [#83] Moved model extensions to the beginning of class and repaired indentations.

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,11 +1,11 @@
 class Car < ActiveRecord::Base
-  validates :model, :registration_number, :owner, presence: true
-  dragonfly_accessor :image
-
   extend Dragonfly::Model
   extend Dragonfly::Model::Validations
-    validates_size_of :image, maximum: 200.kilobytes
-    validates_property :format, of: :image, in: ['jpeg', 'png', 'gif']
+  dragonfly_accessor :image
+    
+  validates :model, :registration_number, :owner, presence: true
+  validates_size_of :image, maximum: 200.kilobytes
+  validates_property :format, of: :image, in: ['jpeg', 'png', 'gif']
 
   belongs_to :owner, class_name: "Person"
   has_many :place_rents
