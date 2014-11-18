77172ee1638c8540789d5d377ab1707faf2f168c
Zuzanna Stolińska <zuzanna.st@gmail.com>
2014-11-18 23:19:10 +0100

    [#70] Added method that extracts locale from browser header.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,7 +23,23 @@ class ApplicationController < ActionController::Base
   end
 
   def set_locale
-    I18n.locale = params[:locale] || I18n.default_locale
-    self.default_url_options = { locale: I18n.locale }
+    if params[:locale].blank?
+      I18n.locale = extract_locale_from_accept_language_header
+    else
+      I18n.locale = params[:locale]
+    end
+  end
+
+  def default_url_options(options = {})
+    {locale: I18n.locale}
+  end
+
+  def extract_locale_from_accept_language_header
+    browser_locale = request.env['HTTP_ACCEPT_LANGUAGE'].try(:scan, /^[a-z]{2}/).try(:first).try(:to_sym)
+    if I18n.available_locales.include? browser_locale
+      browser_locale
+    else
+      I18n.default_locale
+    end
   end
 end
